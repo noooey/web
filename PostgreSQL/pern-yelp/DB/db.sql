691802b00a9de9a98ea8ff67b52eb810cfb2fa45
kueyeon0429 <20203065@kookmin.ac.kr>
2022-04-25 14:43:26 +0900

    :tada: start pern-yelp project

---
+++ PostgreSQL/pern-yelp/DB/db.sql
@@ -0,0 +1,26 @@
+-- for help   \?
+
+-- list database   \l
+
+-- Create database CREATE DATABASE database_name;
+
+-- list all tables   \d
+
+CREATE TABLE products (
+    id INT,
+    name VARCHAR(50),
+    price INT,
+    on_sale boolean
+);
+
+ALTER TABLE products ADD COLUMN featured boolean;
+ALTER TABLE products DROP COLUMN featured;
+
+CREATE TABLE restaurants (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    location VARCHAR(50) NOT NULL,
+    price_range INT NOT NULL check(price_range >= 1 and price_range <= 5)
+);
+
+INSERT INTO restaurants (id, name, location, price_range) values (123, 'mcdonalds', 'new yorks', 3);
