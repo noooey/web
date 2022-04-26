185686e0dc19b45a4896499faec71480ac8ec3df
kueyeon0429 <20203065@kookmin.ac.kr>
2022-04-26 12:42:21 +0900

    :hammer: Configure Routes

--- PostgreSQL/pern-yelp/server/server.js
+++ PostgreSQL/pern-yelp/server/server.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/api/v1/restaurants", (req, res) => {
+    console.log("route handler ran");
     res.status(200).json({
         staus: "success",
         data: {
@@ -19,7 +23,32 @@ app.get("/api/v1/restaurants/:restaurantid", (req, res) => {
 
 // create a restaurant
 app.post("/api/v1/restaurants/", (req, res) => {
-    console.log(req);
+    console.log(req.body);
+    res.status(201).json({
+        status: "success",
+        data: {
+            restaurant: "mcdonalds",
+        },
+    });
+});
+
+// Update Restaurants
+app.put("/api/v1/restaurants/:id", (req, res) => {
+    console.log(req.params.id);
+    console.log(req.body);
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurant: "mcdonalds",
+        },
+    });
+});
+
+// Delete Restaurant
+app.delete("/api/v1/restaurants/:id", (req, res) => {
+    res.status(204).json({
+        status: "success",
+    });
 })
 
 const port = process.env.PORT || 3001;
